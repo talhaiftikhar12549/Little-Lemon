f284038f6feb7df00c7bf693d524ea22d4a57ffd
Talha <talhaiftikhar12549@gmail.com>
2024-09-20 01:19:00 +0500

    Added Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './Redux/Slice/counterSlice'
+
 
 function App() {
 
-
+    const count = useSelector((state) => state.counter.value)
   return (
     <>
         <h1 className="text-3xl font-bold underline">
-            Hello world!
+            Hello world! {count}
         </h1>
     </>
   )
