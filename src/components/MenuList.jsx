d996e50ff18f61d3b65e75f648b9be1223dcae77
Talha <talhaiftikhar12549@gmail.com>
2024-09-21 12:59:53 +0500

    Added Menu button

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -3,10 +3,10 @@ import React from "react"
 export default function MenuList() {
     return (
         <>
-            <div className={"vw-[80%]"}>
-                <div>
+            <div className={" py-4 px-[10%]"}>
+                <div className={""}>
                     <p className={"font-bold text-2xl"}>Order for Delivery!</p>
-                    <div className={"flex justify-around"}>
+                    <div className={"flex justify-around py-4"}>
                         <button className={"bg-[#EBEDEC] font-semibold py-2 px-5 rounded-xl "}>Lunch</button>
                         <button className={"bg-[#EBEDEC] font-semibold py-2 px-5 rounded-xl "}>Main</button>
                         <button className={"bg-[#EBEDEC] font-semibold py-2 px-5 rounded-xl "}>Desert</button>
@@ -16,9 +16,24 @@ export default function MenuList() {
 
                     </div>
                 </div>
-                <div>
+                <section>
+                    <div>
 
-                </div>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </section>
+
             </div>
         </>
     )
