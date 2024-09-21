c4dc1eff2e1602b1ddfbd67ffaf3e25e9d81828a
Talha <talhaiftikhar12549@gmail.com>
2024-09-21 16:40:46 +0500

    Footer Added

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import footerImage from "../assets/Asset 20@4x.png"
 export default function FooterBar()
 {
     return (
-        <footer>
-            <div className={"border-t-[2px] border-[#495E57] py-8"}>
-                © 2023 Little Lemon Resturant. All Rights Reserved
+        <footer className={"relative py-2"}>
+            <div className={"border-t-[2px] border-[#495E57] py-8 flex items-center justify-center"}>
+                    <p className={"text-[#495E57]"}>
+                        © 2023 Little Lemon Resturant. All Rights Reserved
+                    </p>
             </div>
+
+            <img className={"w-[50px] h-[70px] absolute top-5 right-10"} src={footerImage}/>
         </footer>
     )
 }
